bc5b3fc9c811d6120367f59510d9201c498af157
Frankie <ffivarra@yahoo.com>
2015-11-16 16:51:55 -0800

    Initial test page created.

---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+<h1>Imagine this is a cool webpgage.</h1>
+<p>Version 2.0</p>
+<p>Clever content goes here...</p>
+<p>Clever content goes here...</p>
+<p>Clever content goes here...</p>
+</body>
+</html>
